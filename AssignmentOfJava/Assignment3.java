71c44abf4b0518cbdf4894517cf1d80e5b306378
AKASH-098765 <128738064+AKASH-098765@users.noreply.github.com>
2023-03-31 20:47:25 +0530

    Add files via upload

---
+++ AssignmentOfJava/Assignment3.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+
+        //que1: GET number form user and find given number is +ve or -ve and display the result
+
+        // System.out.println("Enter a number: ");
+        // int num=in.nextInt();
+        // if(num<0)
+        //     System.out.println("Given number is Negative");
+        // else
+        //     System.out.println("The number is Positive");
+
+
+
+
+        //que2: program to get age of person and find the age group of that person
+
+    //     System.out.println("Enter your age:");
+    //     int age=in.nextInt();
+    //     if(age<=3)
+    //     System.out.println("you are a Toddlers");
+    // else if(age>=4 && age <=8)
+    //     System.out.println("you are a Kid");
+    // else if(age>=9 && age <=12)
+    //     System.out.println("you are a Child");
+    // else if(age>=13 && age <=18)
+    //     System.out.println("you are a Teen");
+    // else if(age>=4 && age <=8)
+    //     System.out.println("you are a Kid");
+    // else if(age>=19 &&age <=40)
+    //     System.out.println("you are a Adult");
+    // else if(age>=41 && age <=60)
+    //     System.out.println("you are a Matured Adult");
+          
+    // else if(age>=61 &&age <=75)
+    //     System.out.println("you are a Seniour");
+    // else if(age<=76)
+    //     System.out.println("you are Super Seniour");
+
+
+
+    //que3: print number from 1 to 110 and 11 number per line.print coza in place of number which is multiple of 3, loza for multiple of 5,and woza for multiple of 7, print cozaLoza for multiple of 3 and 5
+    
+        // for (int j=1;j<=110;j++){
+            
+        //      if(j%3==0 && j%5==0)
+        //         System.out.print(" cozaloza");
+        //     else if(j%3==0)
+        //         System.out.print(" coza ");
+        //     else if(j%5==0 )
+        //         System.out.print(" loza ");
+        //     else  if(j%7==0)
+        //         System.out.print(" woza ");
+        //     else if (j%11==0)
+        //         System.out.println(j);
+        //     else
+        //         System.out.print(" "+j+" ");    
+        // }     
+        
+        
+            //QUE4:get the number and find weathe it is prime or not
+    //         System.out.print("Enter any number:");
+
+    //         int prime=in.nextInt();
+    //         boolean isPrime=false;
+    //         for(int i=2;i<prime/2;i++){
+    //            if (prime%i==0)
+    //                 isPrime=true;
+    //         }
+    //     if (isPrime)
+    //         System.out.println("This is NOT prime number");
+            
+    //     else
+    //         System.out.println("this is prime number");
+    
+
+
+    // //que5: print a pyramid of *
+    // System.out.print("enter number within 1 to 9:");
+
+    // int num=in.nextInt();
+    // for (int i=0;i<=num;i++){
+    //     for (int k=0;k<num-i;k++){
+    //         System.out.print(" ");
+    //     }
+    //     for (int j=0;j<i;j++){
+    //         System.out.print("*"+" ");
+    //     }
+    //     System.out.println();
+    // }
+
+
+
+    // //que6: program to read month and day and as per input it will return equivalent zodiac sign
+    
+    // System.out.println("Enter the month:");
+    // int month=in.nextInt();
+    
+    // System.out.println("Enter the Date:");
+    // int date=in.nextInt(); 
+    // if(month==1 && date<=20 || month==2 && date>=18)
+    //         System.out.println("Aquarius");
+    // if(month==2 && date<=19 || month==3 && date>=19)
+    //         System.out.println("Pisces");
+    // if(month==3 && date<=21 || month==4 && date>=19)
+    //         System.out.println("Aries");
+    // if(month==4 && date<=20 || month==5 && date>=20)
+    //         System.out.println("Taurus");  
+    // if(month==5 && date<=21 || month==6 && date>=20)
+    //         System.out.println("Gemini");
+    
+    // if(month==6 && date<=21 || month==7 && date>=22)
+    //         System.out.println("Cancer");
+
+    // if(month==7 && date<=23 || month==8 && date>=22)
+    //         System.out.println("Leo");
+    // if(month==8 && date<=20 || month==9 && date>=18)
+    //         System.out.println("Virgo");
+    // if(month==9 && date<=23 || month==10 && date>=22)
+    //         System.out.println("Libra");  
+    // if(month==10 && date<=23 || month==11 && date>=21)
+    //         System.out.println("Scorpio");
+    // if(month==11 && date<=22 || month==12 && date>=21)
+    //         System.out.println("Sagittarius");
+    // if(month==12 && date<=22 || month==1 && date>=19)
+    //         System.out.println("Capricorn");
+}          
+    
+}
