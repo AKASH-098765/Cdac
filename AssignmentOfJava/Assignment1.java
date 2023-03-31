b3b9a921ad5b44c2765de3e1c792e050fd53a24f
Akash <128738064+AKASH-098765@users.noreply.github.com>
2023-03-31 20:55:39 +0530

    Update Assignment1.java

--- AssignmentOfJava/Assignment1.java
+++ AssignmentOfJava/Assignment1.java
@@ -9,74 +9,93 @@ public class Assignment1 {
     static double d;
     public static void main(String[] args) {
         
-    //  // que1: write java program to print hello world
-    //  System.out.println("Hello World!");
+     // que1: write java program to print hello world 
+     System.out.println("Hello World!");
 
-    // // que2: add two number/binary numbers/Character
-    // int a=1,b=2,sum;
-    // sum=a+b;
-    // System.out.println("the sum is:"+sum);
-
+    // que2: add two number/binary numbers/Character
+        
+   /* int a=1,b=2,sum;
+    sum=a+b;
+    System.out.println("the sum is:"+sum);
+    */
     
-    // // que3: calculate compound Interest (formula  ci=principal(1+(rate/interest compounded per year))time)
-    //     double CI=0,principal=100000,Interest=5,time=3;
+    // que3: calculate compound Interest (formula  ci=principal(1+(rate/interest compounded per year))time)
+        
+       /* double CI=0,principal=100000,Interest=5,time=3;
+        CI=principal*(Math.pow((1+Interest/100), time))-principal;
+        System.out.println("the compound interest for given parameter is :"+CI);
+        */
 
-    //     CI=principal*(Math.pow((1+Interest/100), time))-principal;
-    //     System.out.println("the compound interest for given parameter is :"+CI);
+       // que4: calculate power of number
+        
+       /* int num=5,power=4;
+        System.out.println("power is:"+((int)(Math.pow(num, power))));
+        */
+        
+        
+        
+       // que5: swap two number
+        
+       /* int a=11,b=22,temp;
+       System.out.println("number before swap is a="+a+"and b="+b);
+       temp=b;
+       b=a;
+       a=temp;
+       System.out.println("number after swap is a="+a+"and b="+b);
+        */
 
 
-    //    // que4: calculate power of number
-    //     int num=5,power=4;
-    //     System.out.println("power is:"+((int)(Math.pow(num, power))));
+       // que6: calculate area of ractangle
+        
+        /* int length=4,breadth=5;
+        System.out.println("the area of ractangle is "+(length*breadth));
+        */
+        
 
-    //    // que5: swap two number
-    //    int a=11,b=22,temp;
-    //    System.out.println("number before swap is a="+a+"and b="+b);
-    //    temp=b;
-    //    b=a;
-    //    a=temp;
-    //    System.out.println("number after swap is a="+a+"and b="+b);
-
-    //    // que6: calculate area of ractangle
-    //     int length=4,breadth=5;
-    //     System.out.println("the area of ractangle is "+(length*breadth));
-
-
-    //    // que7: calculate area of circumference of circle using multiple class
-    //    int redius=4;
-    //    double circumference=0;
-    //    circumference=2*3.1415*redius;
-    //    System.out.println("circumference of circle is :"+circumference);
-
-    //    // que8: find ASCII value of a Character
-    //    int i='c';
-    //    System.out.println("the ASCII value is "+i);
-
-    //    // que9: print default value of primitive data type var.
+       // que7: calculate area of circumference of circle using multiple class
        
-    //    System.out.println("default value of int is:"+i);
-    //    System.out.println("default value of float is:"+f);
-    //    System.out.println("default value of double is:"+d);
-    //    System.out.println("default value of short is:"+s);
-    //    System.out.println("default value of long is:"+l);
-    //    System.out.println("default value of byte is:"+b);
-    //    System.out.println("default value of boolean is:"+bl);
+        /* int redius=4;
+       double circumference=0;
+       circumference=2*3.1415*redius;
+       System.out.println("circumference of circle is :"+circumference);
+        */
 
-    //    // que10: swap two var without using the third var
-    //    int a=11,b=22;
-    //    System.out.println("the number before swap is: a="+a+" and b= "+b);
-    //    a=a+b;
-    //    b=a-b;
-    //    a=a-b;
-    //    System.out.println("the number after swap is: a="+a+" and b= "+b);
 
+       // que8: find ASCII value of a Character
+      /* int i='c';
+       System.out.println("the ASCII value is "+i);
+        */
+        
+        
+       // que9: print default value of primitive data type var.
        
-    //    // que11: print fibonachi series till n
-    //     int n=10,febo=0;
-    //     for (int i=1;i<=n;i++){
-    //         febo=febo+i;
-    //     }
-    //     System.out.println("the febonachi of "+n+ " is "+febo);
-
+       /* System.out.println("default value of int is:"+i);
+       System.out.println("default value of float is:"+f);
+       System.out.println("default value of double is:"+d);
+       System.out.println("default value of short is:"+s);
+       System.out.println("default value of long is:"+l);
+       System.out.println("default value of byte is:"+b);
+       System.out.println("default value of boolean is:"+bl);
+        */
+        
+        
+       // que10: swap two var without using the third var
+      /* int a=11,b=22;
+       System.out.println("the number before swap is: a="+a+" and b= "+b);
+       a=a+b;
+       b=a-b;
+       a=a-b;
+       System.out.println("the number after swap is: a="+a+" and b= "+b);
+        */
+        
+       
+       // que11: print fibonachi series till n
+        /*int n=10,febo=0;
+        for (int i=1;i<=n;i++){
+            febo=febo+i;
+        }
+        System.out.println("the febonachi of "+n+ " is "+febo);
+        */
+        
     }
 }
